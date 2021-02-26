6c8d2a1b0d3cbb2d404f156b4d067a293f34ab53
carbanak666 <56926537+carbanak666@users.noreply.github.com>
2021-02-26 19:28:41 -0300

    Add files via upload

---
+++ realloc.c
@@ -0,0 +1,24 @@
+//realloc = relocating vai re alocar a memoria
+//vou deixar a documentação no github
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int *m = (int *) malloc(sizeof(int));
+
+	*m = 10; //setando o valor do malloc
+
+	printf("%i\n", *m); //printando o tamanho da memoria alocada
+
+    int *f = (int *) realloc(m, sizeof(int));
+
+    *f = 30; //setando o valor do realloc
+
+	printf("%i\n", *f); //printando o tamanho da memoria alocada
+
+	free(m); //devolve a memoria pro sistema
+	f = NULL; //devolvendo a memoria para o sistema um codigo não pode usar 2 free() ou mais
+
+	return 0;
+}
